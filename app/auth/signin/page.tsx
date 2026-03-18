0939559fd89765edfb71a04478439d36f99eccb6
pabitrabarua123 <pabitrabesus@gmail.com>
2026-03-18 09:59:33 +0530

    fix auth 4

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
 import SignInForm from "@/components/auth/SignInForm";
 
-export default function SignInPage() {
-  return (
-    <main>
-      <Suspense fallback={<div>Loading...</div>}>
-        <SignInForm />
-      </Suspense>
-    </main>
-  );
+export default function SignInPage({
+  searchParams,
+}: {
+  searchParams: { referrer_template?: string };
+}) {
+  return <SignInForm referrer_template={searchParams.referrer_template} />;
 }
